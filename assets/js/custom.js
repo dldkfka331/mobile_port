27575fe4d73f63fad6ef00b1cee8556f4a4109c2
dldkfka331 <56900372+dldkfka331@users.noreply.github.com>
2020-02-27 12:40:46 +0900

    1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,18 +3,25 @@ $(function(){
     
     //로딩
     let mainText = $(".main_tit > h1").text().split('').join("</span><span>");
-        mainText ="<span>"+ mainText +"</span>";
-        $(".main_tit > h1").html(mainText);
-        $(".main_tit > h1").find("span").each(function(index){
-            setTimeout(function(){
-                $(".main_tit > h1").find("span").eq(index).show();
-            },100*(index+1));
-        });
+    mainText ="<span>"+ mainText +"</span>";
+    $(".main_tit > h1").html(mainText);
+    $(".main_tit > h1").find("span").each(function(index){
+        setTimeout(function(){
+            $(".main_tit > h1").find("span").eq(index).show();
+        },100*(index+1));
+    });
 
+    setTimeout(function(){
+        $(".main_box > div").each(function(index){
+            setTimeout(function(){
+                $(".main_box > div").eq(index).show();
+                $(".main_box > div").eq(-index).show();
+            },100*(index+1));
+        });    
         setTimeout(function(){
             $(".main_row").fadeIn();
         },2500);
-
+    }, 3000);
 
     //메인 화살표
     setInterval(down,1000);
